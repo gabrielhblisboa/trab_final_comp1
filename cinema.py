d46d8a193e73d69ca5d2a90182005e734a3cda5b
Gabriel <gabrielhblisboa@outlook.com>
2020-11-13 19:07:24 -0300

    reserva feita. comentarios adicionados. mensagens para o usuario atualizadas

--- cinema.py
+++ cinema.py
@@ -1,7 +1,7 @@
 import random as rnd
 
 def menuprincipal():
-    print('1 - Ver sala')
+    print('1 - Ver sala/Reservar assento')
     print('2 - Apagar reservas')
     print('3 - Carregar salas salvas')
     print('4 - Sortear assentos promocionais')
@@ -9,7 +9,9 @@ def menuprincipal():
 
 
 def mostrarSalas(nomeArq):
-    # Mostra as salas disponiveis
+    '''
+    A função lê o arquivo e imprime as salas disponíveis.
+    '''
     salas = open(nomeArq, 'r')
     sala = salas.readline()
     i = 1
@@ -20,22 +22,28 @@ def mostrarSalas(nomeArq):
         sala = salas.readline()
     print()
     salas.close()
-    '''
-    Numero de salas e igual a i - 1 pois o i e
-    contado mais uma vez antes de sair do while
-    '''
+    
+    # Número de salas é igual a i - 1 pois o i é
+    # contado mais uma vez antes de sair do while
+    
     nSalas = i - 1
     return nSalas
 
 
 def escolherSala(nomeArq):
-    # n = numero de salas
-    n = mostrarSalas(nomeArq)
+    '''
+    Função que interage com o usuário para que ele escolha
+    a sala. Após a escolha da sala, ela é transformada de
+    string para lista de listas.
+    '''
+    # nSalas = número de salas
+    
+    nSalas = mostrarSalas(nomeArq)
     while True:
         s = input('Digite o numero da sala: ')
         try:
             s = int(s)
-            if s < 0 or s > n:
+            if s < 0 or s > nSalas:
                 raise ValueError 
         except ValueError:
             print('Valor invalido. Tente novamente.')
@@ -54,14 +62,21 @@ def escolherSala(nomeArq):
     print('1 = assento reservado')
     print('2 = assento promocional')
     salas.close()
-    '''
-    i -> indica o numero da sala e a posicao da
-    sala dentro do arquivo
-    '''
+    
+    # i -> indica o número da sala e a posição da
+    # sala dentro do arquivo
+    
     return sala, i
 
 
 def reservarAssento(nomeArq):
+    '''
+    A função interage com o usuário para que ele digite o
+    número da fileira e do assento que ele deseja
+    reservar. O valor do assento passa de 0
+    (assento livre) para 1 (assento reservado). A sala 
+    (lista de listas) é modificada e salva no arquivo.
+    '''
     sala, i = escolherSala(nomeArq)
     op = input('Deseja reservar um assento? s/n: ')
     while op != 'n':
@@ -100,7 +115,7 @@ def reservarAssento(nomeArq):
 
 def carregarSalas():
     '''
-    Funcao que carrega um determinado arquivo salvo
+    Função que carrega um determinado arquivo salvo
     '''
     nomeArq = input('Digite o nome do arquivo'+\
                     '(ex: salas.txt): ')
@@ -116,6 +131,12 @@ def carregarSalas():
 
 
 def salvarSalas(nomeArq, sala, linha):
+    '''
+    Função que salva no arquivo as salas modificadas 
+    sala -> lista de listas que representa uma sala
+    linha -> indica em qual linha está a sala que tem
+             que ser modificada
+    '''
     salas = open(nomeArq, 'r+')
     n = 1
     l = salas.readline()
@@ -133,37 +154,57 @@ def salvarSalas(nomeArq, sala, linha):
 
 
 def apagarReservas(nomeArq):
+    '''
+    Função que apaga todas as reservas de todas
+    as salas
+    '''
     salas = open(nomeArq, 'r')
     salas_w = open(nomeArq, 'r+')
+    
+    # pos -> vai servir para mover o cursor pro início de
+    # cada linha na hora de sobrescrevê-la
+    
     pos = salas.tell()
     sala = salas.readline()
     while sala != '':
         sala = eval(sala)
-        print(sala)
         for f_pos in range(len(sala)):
             fileira = sala[f_pos]
             for a_pos in range(len(fileira)):
                 assento = fileira[a_pos]
                 if assento != 0:
-                    sala[f_pos][a_pos] = 0
-        print(sala)         
+                    sala[f_pos][a_pos] = 0         
         salas_w.seek(pos)
         salas_w.write(str(sala))
         pos = salas.tell()
         sala = salas.readline()
+    print('Reservas apagadas com sucesso!')
+    print()
     salas.close()
     salas_w.close()
 
 
 def sortearAssento(nomeArq):
+    '''
+    Função que sorteia um determinado número de assentos
+    promocionais. A função interage com o usuário para
+    determinar quantos assentos serão sorteados, e verifica
+    se há assentos disponíveis para fazer o sorteio. Os
+    assentos promocionais são marcados com o numero 2.
+    '''
     sala, linha = escolherSala(nomeArq)
-    op = ''
-    while op != 'n':
-        numSort = input('Digite o numero de  assentos que deseja  sortear: ')
-        try:
-            numSort = int(numSort)
-            if numSort <= 0:
-                raise ValueError
+    op = 's'
+    while op != 'n': 
+        while True:
+            numSort = input('Digite o numero de assentos que deseja sortear: ')
+            try:
+                numSort = int(numSort)
+                if numSort <= 0:
+                    raise ValueError
+            except ValueError:
+                print('Valor invalido. Tente novamente.')
+            else:
+                break   
         somaTotal = 0
         for f in sala:
             somaFileira = 0
@@ -172,13 +213,25 @@ def sortearAssento(nomeArq):
                     somaFileira += 1
             somaTotal += somaFileira
         if somaTotal < numSort:
-            print('So ha %d assentos disponiveis.'%(somaTotal))
+            print('Esta sala possui %d assentos disponiveis.'%(somaTotal))
             print('Nao foi possivel fazer o sorteio.')
-            op = input('Deseja realizar um novo sorteio? (s/n): ')
+            while True:
+                op = input('Deseja realizar um novo sorteio? (s/n): ')
+                if op == 'n':
+                    return None
+                elif op != 's' and op != 'n':
+                    print('Opcao invalida. Tente novamente.')
+                else:
+                    break
         else:
-            op = 'n'    
+            break        
     n = 1
     while n <= numSort:
+        
+        # A cada iteração é sorteada uma fileira e um
+        # assento, e o sorteio só é considerado se o assento
+        # estiver livre.
+        
         fileira = rnd.randint(0, len(sala)-1)
         assento = rnd.randint(0, len(sala[fileira])-1)
         if sala[fileira][assento] == 0:
@@ -191,7 +244,14 @@ def sortearAssento(nomeArq):
 
 
 def main():
-    salas = 'salas.txt'
+    while True:
+    
+        # A função carregarSalas fica em loop até o
+        # usuário digitar um nome de arquivo válido.
+        
+        salas = carregarSalas()
+        if salas is not None:
+            break
     while True:
         menuprincipal()
         op = input('Digite uma das opcoes acima: ')
@@ -200,11 +260,11 @@ def main():
         elif op == '2':
             apagarReservas(salas)
         elif op == '3':
-            '''
-            caso o nome do arquivo digitado nao seja
-            encontrado, o arquivo que ja estava sendo
-            utilizado e mantido
-            '''
+
+            # Caso o nome do arquivo digitado não seja
+            # encontrado, o arquivo que já estava sendo
+            # utilizado é mantido
+            
             tmpSala = carregarSalas()
             if tmpSala is not None:
                 salas = tmpSala
